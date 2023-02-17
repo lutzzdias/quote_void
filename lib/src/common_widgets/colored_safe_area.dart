aaf68c7a3420152593b232d70480ca97f06e2948
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-17 00:05:45 -0400

    Create coloredSafeArea common widget

---
+++ lib/src/common_widgets/colored_safe_area.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ColoredSafeArea extends StatelessWidget {
+  final Widget child;
+  final Color? color;
+  const ColoredSafeArea({
+    super.key,
+    required this.child,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color ?? Theme.of(context).appBarTheme.backgroundColor,
+      child: SafeArea(
+        child: Container(
+          color: Theme.of(context).colorScheme.background,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
