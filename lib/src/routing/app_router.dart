e32369ec725d0d83a3b99b2359213be7dc490204
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-16 23:15:29 -0400

    Add research results about topics I did not know

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -9,9 +9,10 @@ enum AppRoute {
 final goRouter = GoRouter(
   initialLocation: '/home',
   debugLogDiagnostics: true,
-  // TODO: Search about redirect
+  // TODO: Implement redirect logic for when the user tries to access routes and is not logged in
   // redirect: (context, state) => null,
-  // TODO: Search about refreshListenable
+  // Changes on the listenable will cause the router to refresh it's route
+  // TODO: Implement provider which will notify when the user is updated
   // refreshListenable: null,
   routes: [
     GoRoute(
@@ -19,8 +20,8 @@ final goRouter = GoRouter(
       name: AppRoute.home.name,
       pageBuilder: (context, state) => MaterialPage(
         key: state.pageKey,
-        // TODO: Search about fullscreenDialog
-        fullscreenDialog: true,
+        // fullscreenDialog -> makes the appBar have a close button instead of a back button
+        // on iOS the screen is not closeable with the swipe gesture
         child: HomeScreen(),
       ),
     )
