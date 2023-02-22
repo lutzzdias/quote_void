18a522d979059cd86017cf223bfc0d00d008dbe4
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-22 00:59:34 -0400

    Add transparent color to constants

--- lib/src/constants/theme/app_colors.dart
+++ lib/src/constants/theme/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const black = Color(0xFF000000);
   static const white = Color(0xFFFFFFFF);
   static const gray = Color(0xFFF2F1F1);
+  static const transparent = Color(0x00FFFFFF);
 }
