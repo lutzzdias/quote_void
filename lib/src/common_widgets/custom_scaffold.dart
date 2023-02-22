3d7729e270b273a63c538e2c506f536149f76855
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-22 01:08:34 -0400

    Improve reusability of base widget (customScaffold)

---
+++ lib/src/common_widgets/custom_scaffold.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/material.dart';
+import 'package:quote_void/src/constants/app_sizes.dart';
+import 'package:quote_void/src/constants/theme/app_colors.dart';
+
+typedef ResponsiveBuilder = Widget Function(
+  BuildContext context,
+  Size size,
+);
+
+class CustomScaffold extends StatelessWidget {
+  final Color? color;
+  final PreferredSizeWidget? appBar;
+  final Widget body;
+  final Widget? floatingActionButton;
+
+  const CustomScaffold({
+    super.key,
+    this.color,
+    this.appBar,
+    required this.body,
+    this.floatingActionButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color ?? AppColors.gray,
+      child: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) => Scaffold(
+            appBar: appBar,
+            floatingActionButton: floatingActionButton,
+            body: SingleChildScrollView(
+              child: SizedBox(
+                height: constraints.maxHeight,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Sizes.p20,
+                    vertical: Sizes.p12,
+                  ),
+                  child: body,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/features/authentication/presentation/sign_in/sign_in_screen.dart
+++ lib/src/features/authentication/presentation/sign_in/sign_in_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:quote_void/src/common_widgets/custom_safe_area.dart';
+import 'package:quote_void/src/common_widgets/custom_scaffold.dart';
 import 'package:quote_void/src/constants/app_sizes.dart';
 import 'package:quote_void/src/constants/theme/app_button_style.dart';
 import 'package:quote_void/src/constants/theme/app_text_style.dart';
@@ -10,130 +10,127 @@ class SignInScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return CustomSafeArea(
-      builder: (context, size) {
-        return Scaffold(
-          body: SingleChildScrollView(
-            child: SizedBox(
-              height: size.height,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.p20,
-                  vertical: Sizes.p12,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Spacer(
-                      flex: 15,
-                    ),
-                    AspectRatio(
-                      aspectRatio: 3,
-                      child: Image.asset(
-                        'assets/logo.png',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    gapH12,
-                    const Text(
-                      'Quote Void',
-                      textAlign: TextAlign.center,
-                      style: AppTextStyle.title,
-                    ),
-                    Spacer(
-                      flex: 20,
-                    ),
-                    const TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Email address or username',
-                      ),
-                    ),
-                    gapH16,
-                    const TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        suffixIcon: Icon(Icons.remove_red_eye),
-                      ),
-                    ),
-                    gapH12,
-                    const Align(
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        'Forgot password',
-                        style: AppTextStyle.link,
-                      ),
-                    ),
-                    Spacer(
-                      flex: 15,
-                    ),
-                    SizedBox(
-                      height: Sizes.p48,
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        style: AppButtons.outlinedButtonStyle,
-                        child: const Text(
-                          'Sign In',
-                          style: AppTextStyle.titleSmall,
-                        ),
-                      ),
-                    ),
-                    gapH12,
-                    const Text(
-                      'Or',
-                      style: AppTextStyle.bodyTranslucent,
-                      textAlign: TextAlign.center,
-                    ),
-                    gapH12,
-                    SizedBox(
-                      height: Sizes.p48,
-                      child: OutlinedButton.icon(
-                        onPressed: () {},
-                        icon: const Icon(Icons.abc),
-                        label: const Text(
-                          'Google',
-                          style: AppTextStyle.subtitle,
-                        ),
-                        style: AppButtons.outlinedButtonStyle,
-                      ),
-                    ),
-                    gapH16,
-                    SizedBox(
-                      height: Sizes.p48,
-                      child: OutlinedButton.icon(
-                        onPressed: () {},
-                        icon: const Icon(Icons.abc),
-                        label: const Text(
-                          'Apple ID',
-                          style: AppTextStyle.subtitle,
-                        ),
-                        style: AppButtons.outlinedButtonStyle,
-                      ),
-                    ),
-                    Spacer(
-                      flex: 35,
-                    ),
-                    RichText(
-                      textAlign: TextAlign.center,
-                      text: const TextSpan(
-                        text: 'Don\'t have an account? ',
-                        style: AppTextStyle.body,
-                        children: [
-                          TextSpan(
-                            text: 'Sign up',
-                            style: AppTextStyle.linkBold,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+    return CustomScaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Spacer(
+            flex: 15,
+          ),
+          AspectRatio(
+            aspectRatio: 3,
+            child: Image.asset(
+              'assets/logo.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          gapH12,
+          const Text(
+            'Quote Void',
+            textAlign: TextAlign.center,
+            style: AppTextStyle.title,
+          ),
+          const Spacer(
+            flex: 25,
+          ),
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'Email address or username',
+            ),
+          ),
+          gapH16,
+          // TODO: Create password field widget
+          const TextField(
+            keyboardType: TextInputType.visiblePassword,
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              suffixIcon: Icon(Icons.remove_red_eye),
+            ),
+          ),
+          gapH12,
+          Align(
+            alignment: Alignment.centerRight,
+            child: GestureDetector(
+              onTap: () => debugPrint('clicked on forgot password'),
+              child: Text(
+                'Forgot password?',
+                style: AppTextStyle.link,
               ),
             ),
           ),
-        );
-      },
+          const Spacer(
+            flex: 20,
+          ),
+          SizedBox(
+            height: Sizes.p48,
+            child: OutlinedButton(
+              onPressed: () {},
+              style: AppButtons.outlinedButtonStyle,
+              child: const Text(
+                'Sign In',
+                style: AppTextStyle.titleSmall,
+              ),
+            ),
+          ),
+          gapH12,
+          const Text(
+            'Or',
+            style: AppTextStyle.bodyTranslucent,
+            textAlign: TextAlign.center,
+          ),
+          gapH12,
+          SizedBox(
+            height: Sizes.p48,
+            child: OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.abc),
+              label: const Text(
+                'Google',
+                style: AppTextStyle.subtitle,
+              ),
+              style: AppButtons.whiteOutlinedButtonStyle,
+            ),
+          ),
+          gapH16,
+          SizedBox(
+            height: Sizes.p48,
+            child: OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.abc),
+              label: const Text(
+                'Apple ID',
+                style: AppTextStyle.subtitle,
+              ),
+              style: AppButtons.whiteOutlinedButtonStyle,
+            ),
+          ),
+          const Spacer(
+            flex: 35,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Don\'t have an account? ',
+                style: AppTextStyle.body,
+              ),
+              GestureDetector(
+                onTap: () => debugPrint('clicked on sign up'),
+                child: const Text(
+                  'Sign up',
+                  style: AppTextStyle.linkBold,
+                ),
+              ),
+            ],
+          ),
+          Spacer(
+            flex: 5,
+          ),
+        ],
+      ),
     );
   }
 }
