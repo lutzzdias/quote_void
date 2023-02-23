6c6528265487ba4f4c91583676421b572ed17754
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-22 23:57:32 -0400

    Add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quote_void/firebase_options.dart';
 import 'package:quote_void/src/app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
