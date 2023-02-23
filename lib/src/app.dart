ea776fb201bbb2e743ce5dea1616ed9859f23418
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-23 13:39:46 -0400

    Implement goRouter redirect missing logic

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,21 +3,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quote_void/src/constants/theme/app_colors.dart';
 import 'package:quote_void/src/routing/app_router.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        title: 'Quote Void',
-        routerConfig: goRouter,
-        theme: ThemeData(
-          scaffoldBackgroundColor: AppColors.gray,
-          colorScheme: const ColorScheme.light().copyWith(
-            primary: AppColors.black,
-          ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Quote Void',
+      routerConfig: goRouter,
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.gray,
+        colorScheme: const ColorScheme.light().copyWith(
+          primary: AppColors.black,
         ),
       ),
     );
--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:quote_void/src/features/authentication/data/firebase_auth_repository.dart';
 import 'package:quote_void/src/features/authentication/presentation/forgot_password/forgot_password_screen.dart';
 import 'package:quote_void/src/features/authentication/presentation/sign_in/sign_in_screen.dart';
 import 'package:quote_void/src/features/authentication/presentation/sign_up/sign_up_screen.dart';
 import 'package:quote_void/src/features/category/presentation/categories_list/categories_list_screen.dart';
 import 'package:quote_void/src/features/home/home_screen.dart';
+import 'package:quote_void/src/routing/go_router_refresh_stream.dart';
 
 enum AppRoute {
   home,
@@ -14,69 +17,69 @@ enum AppRoute {
   categoryList,
 }
 
-final goRouter = GoRouter(
-  initialLocation: '/',
-  debugLogDiagnostics: true,
+final goRouterProvider = Provider<GoRouter>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return GoRouter(
+    initialLocation: '/',
+    debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final bool isLoggedIn = authRepository.currentUser != null;
 
-  redirect: (context, state) {
-    // TODO: check if user is logged in
-    const bool isLoggedIn = false;
-
-    if (isLoggedIn) {
-      if (state.subloc.startsWith('/authentication')) {
-        return '/';
+      if (isLoggedIn) {
+        if (state.subloc.startsWith('/authentication')) {
+          return '/';
+        }
+      } else if (!state.subloc.startsWith('/authentication')) {
+        return '/authentication/signIn';
+      } else {
+        return state.fullpath;
       }
-    } else if (!state.subloc.startsWith('/authentication')) {
-      return '/authentication/signIn';
-    } else {
-      return state.fullpath;
-    }
-  },
-
-  // TODO: Implement provider which will notify when the user is updated
-  // refreshListenable: null,
-  routes: [
-    GoRoute(
-      path: '/',
-      name: AppRoute.home.name,
-      pageBuilder: (context, state) => MaterialPage(
-        key: state.pageKey,
-        // fullscreenDialog -> makes the appBar have a close button instead of a back button
-        // on iOS the screen is not closeable with the swipe gesture
-        child: const HomeScreen(),
+      return null;
+    },
+    refreshListenable: GoRouterRefreshStream(authRepository.authStateChanges()),
+    routes: [
+      GoRoute(
+        path: '/',
+        name: AppRoute.home.name,
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          // fullscreenDialog -> makes the appBar have a close button instead of a back button
+          // on iOS the screen is not closeable with the swipe gesture
+          child: const HomeScreen(),
+        ),
       ),
-    ),
-    GoRoute(
-      path: '/authentication/signIn',
-      name: AppRoute.signIn.name,
-      pageBuilder: (context, state) => MaterialPage(
-        key: state.pageKey,
-        child: SignInScreen(),
+      GoRoute(
+        path: '/authentication/signIn',
+        name: AppRoute.signIn.name,
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          child: SignInScreen(),
+        ),
       ),
-    ),
-    GoRoute(
-      path: '/authentication/signUp',
-      name: AppRoute.signUp.name,
-      pageBuilder: (context, state) => MaterialPage(
-        key: state.pageKey,
-        child: SignUpScreen(),
+      GoRoute(
+        path: '/authentication/signUp',
+        name: AppRoute.signUp.name,
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          child: SignUpScreen(),
+        ),
       ),
-    ),
-    GoRoute(
-      path: '/authentication/forgotPassword',
-      name: AppRoute.forgotPassword.name,
-      pageBuilder: (context, state) => MaterialPage(
-        key: state.pageKey,
-        child: const ForgotPasswordScreen(),
+      GoRoute(
+        path: '/authentication/forgotPassword',
+        name: AppRoute.forgotPassword.name,
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          child: const ForgotPasswordScreen(),
+        ),
       ),
-    ),
-    GoRoute(
-      path: '/category/list',
-      name: AppRoute.categoryList.name,
-      pageBuilder: (context, state) => MaterialPage(
-        key: state.pageKey,
-        child: CategoriesListScreen(),
+      GoRoute(
+        path: '/category/list',
+        name: AppRoute.categoryList.name,
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          child: CategoriesListScreen(),
+        ),
       ),
-    ),
-  ],
-);
+    ],
+  );
+});
