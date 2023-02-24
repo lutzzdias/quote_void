28d0195f8fe260b404b0c1b4c54eb146e8c8cd0d
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-24 13:38:30 -0400

    Improve SignInController authRepository dependency

--- lib/src/features/authentication/presentation/sign_in/sign_in_controller.dart
+++ lib/src/features/authentication/presentation/sign_in/sign_in_controller.dart
@@ -4,22 +4,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quote_void/src/features/authentication/data/firebase_auth_repository.dart';
 
 class SignInController extends AutoDisposeAsyncNotifier<void> {
+  late final AuthRepository _authRepository;
+
   @override
-  FutureOr<void> build() => null;
+  FutureOr<void> build() => _authRepository = ref.read(authRepositoryProvider);
 
   Future<void> signInWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
-    final authRepository = ref.read(authRepositoryProvider);
     state = const AsyncValue.loading();
-    state = await authRepository.signInWithEmailAndPassword(email, password);
+    state = await _authRepository.signInWithEmailAndPassword(email, password);
   }
 
   Future<void> signInWithGoogle() async {
-    final authRepository = ref.read(authRepositoryProvider);
     state = const AsyncValue.loading();
-    state = await authRepository.signInWithGoogle();
+    state = await _authRepository.signInWithGoogle();
   }
 }
 
