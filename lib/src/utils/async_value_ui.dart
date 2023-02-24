54fca77f4a4e53903dbb49786518c8764bcbc3e1
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-24 14:21:49 -0400

    Improve error snackBar UI

--- lib/src/utils/async_value_ui.dart
+++ lib/src/utils/async_value_ui.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quote_void/src/constants/app_sizes.dart';
+import 'package:quote_void/src/constants/theme/app_colors.dart';
+import 'package:quote_void/src/constants/theme/app_text_style.dart';
 
 typedef VoidAsyncValue = AsyncValue<void>;
 
@@ -8,9 +11,40 @@ extension AsyncValueUI on VoidAsyncValue {
 
   void showSnackBarOnError(BuildContext context) => whenOrNull(
         error: (error, _) {
-          // TODO: Improve snackBar UI
           ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(error.toString())),
+            SnackBar(
+              backgroundColor: AppColors.white,
+              showCloseIcon: true,
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.p8,
+                horizontal: Sizes.p16,
+              ),
+              content: Row(
+                children: [
+                  const Icon(
+                    Icons.error,
+                    color: Colors.black,
+                  ),
+                  gapW12,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Error',
+                          style: AppTextStyle.subtitleBold,
+                        ),
+                        Text(
+                          error.toString(),
+                          style: AppTextStyle.bodyTranslucent,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           );
         },
       );
