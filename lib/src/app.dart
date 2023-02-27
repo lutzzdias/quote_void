42fdc71721f498589dcf9f06fdebd3b0471330fb
lutzzdias <thiagolutzdias@hotmail.com>
2023-02-27 10:58:55 -0400

    Rename MyApp to App

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,8 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quote_void/src/constants/theme/app_colors.dart';
 import 'package:quote_void/src/routing/app_router.dart';
 
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+class App extends ConsumerWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
