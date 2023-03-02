024d18eec03566bc5dcdaf8b9204736a25805eb5
lutzzdias <thiagolutzdias@hotmail.com>
2023-03-02 17:00:47 -0400

    Remove unused imports

--- lib/src/features/authentication/presentation/sign_in/sign_in_controller.dart
+++ lib/src/features/authentication/presentation/sign_in/sign_in_controller.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quote_void/src/features/authentication/application/auth_service.dart';
-import 'package:quote_void/src/features/authentication/data/firebase_auth_repository.dart';
 
 class SignInController extends AutoDisposeAsyncNotifier<void> {
   late final AuthService _authService;
--- lib/src/features/authentication/presentation/sign_in/sign_in_screen.dart
+++ lib/src/features/authentication/presentation/sign_in/sign_in_screen.dart
@@ -6,8 +6,6 @@ import 'package:quote_void/src/common_widgets/custom_scaffold.dart';
 import 'package:quote_void/src/common_widgets/text_with_link.dart';
 import 'package:quote_void/src/constants/app_sizes.dart';
 import 'package:quote_void/src/constants/theme/app_text_style.dart';
-import 'package:quote_void/src/features/authentication/application/auth_service.dart';
-import 'package:quote_void/src/features/authentication/data/firebase_auth_repository.dart';
 import 'package:quote_void/src/features/authentication/presentation/sign_in/widgets/sign_in_form.dart';
 import 'package:quote_void/src/features/authentication/presentation/sign_in/sign_in_controller.dart';
 import 'package:quote_void/src/routing/app_router.dart';
