7b42c7512e265f9ba80a917a0a82858e3a141a40
lutzzdias <thiagolutzdias@hotmail.com>
2023-04-06 20:07:38 -0400

    feat: Add password visibility toggle

--- lib/src/widgets/password_field.dart
+++ lib/src/widgets/password_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PasswordField extends StatelessWidget {
+class PasswordField extends StatefulWidget {
   // TODO: Add state management and possibility of toggle text visibility
   const PasswordField({
     super.key,
@@ -14,17 +14,27 @@ class PasswordField extends StatelessWidget {
   final ValueChanged<String> onChanged;
 
   @override
+  State<PasswordField> createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool isVisible = false;
+
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
       keyboardType: TextInputType.visiblePassword,
-      obscureText: true,
-      decoration: const InputDecoration(
+      obscureText: isVisible,
+      decoration: InputDecoration(
         hintText: 'Password',
-        suffixIcon: Icon(Icons.remove_red_eye),
+        suffixIcon: GestureDetector(
+          onTap: () => setState(() => isVisible = !isVisible),
+          child: const Icon(Icons.remove_red_eye),
+        ),
       ),
-      autovalidateMode: autovalidateMode,
-      onChanged: onChanged,
-      validator: validator,
+      autovalidateMode: widget.autovalidateMode,
+      onChanged: widget.onChanged,
+      validator: widget.validator,
     );
   }
 }
