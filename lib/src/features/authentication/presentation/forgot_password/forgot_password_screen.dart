a4796b4419eb5416a4ddacbdc7475cf9bfe58531
lutzzdias <thiagolutzdias@hotmail.com>
2023-04-17 00:05:10 -0400

    refactor: use responsiveCenter in forgotPassword screen

--- lib/src/features/authentication/presentation/forgot_password/forgot_password_screen.dart
+++ lib/src/features/authentication/presentation/forgot_password/forgot_password_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quote_void/src/widgets/custom_outlined_button.dart';
-import 'package:quote_void/src/widgets/custom_scaffold.dart';
+import 'package:quote_void/src/widgets/responsive_center.dart';
 import 'package:quote_void/src/widgets/text_with_link.dart';
 import 'package:quote_void/src/constants/app_sizes.dart';
 import 'package:quote_void/src/constants/theme/app_text_style.dart';
@@ -12,60 +12,52 @@ class ForgotPasswordScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(
-            height: Sizes.p64,
-            child: Image.asset(
-              'assets/images/logo.png',
-              fit: BoxFit.fitHeight,
+    return Scaffold(
+      body: ResponsiveCenter(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: const [
+                Icon(
+                  Icons.lock,
+                  size: Sizes.p32,
+                ),
+                gapW8,
+                Text(
+                  'Forgot password?',
+                  style: AppTextStyle.title,
+                )
+              ],
             ),
-          ),
-          const Spacer(),
-          Row(
-            children: const [
-              Icon(
-                Icons.lock,
-                size: Sizes.p32,
+            gapH32,
+            const Text(
+              'Please enter your email address, so we can send you a link to reset your password.',
+              style: AppTextStyle.body,
+            ),
+            gapH24,
+            const TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: 'Email address',
               ),
-              gapW8,
-              Text(
-                'Forgot password?',
-                style: AppTextStyle.title,
-              )
-            ],
-          ),
-          gapH32,
-          const Text(
-            'Please enter your email address, so we can send you a link to reset your password.',
-            style: AppTextStyle.body,
-          ),
-          gapH24,
-          const TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              hintText: 'Email address',
             ),
-          ),
-          gapH64,
-          CustomOutlinedButton(
-            title: 'Send link',
-            isLoading: false,
-            // TODO: implement send recover link logic
-            onPressed: () => debugPrint('Click on send link'),
-          ),
-          gapH32,
-          TextWithLink(
-            text: 'Return to ',
-            linkText: 'Sign in',
-            onTap: () => context.goNamed(AppRoute.signIn.name),
-          ),
-          // Space to fix alignment due to logo size on top
-          gapH64,
-          const Spacer(),
-        ],
+            gapH32,
+            CustomOutlinedButton(
+              title: 'Send link',
+              isLoading: false,
+              // TODO: implement send recover link logic
+              onPressed: () => debugPrint('Click on send link'),
+            ),
+            gapH12,
+            TextWithLink(
+              text: 'Return to ',
+              linkText: 'Sign in',
+              onTap: () => context.goNamed(AppRoute.signIn.name),
+            ),
+          ],
+        ),
       ),
     );
   }
