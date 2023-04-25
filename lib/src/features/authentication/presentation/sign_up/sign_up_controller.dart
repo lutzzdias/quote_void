1b74bdd112eaf00e2c532e82ae4e3bbca85e0c07
lutzzdias <thiagolutzdias@hotmail.com>
2023-04-25 00:00:43 -0400

    style: change - to _ in picName in firebaseStorage

--- lib/src/features/authentication/presentation/sign_up/sign_up_controller.dart
+++ lib/src/features/authentication/presentation/sign_up/sign_up_controller.dart
@@ -22,7 +22,7 @@ class SignUpController extends AutoDisposeAsyncNotifier<void> {
     TaskSnapshot task = await FirebaseStorage.instance
         .ref()
         .child('profile-pics')
-        .child('$username-${const Uuid().v4()}')
+        .child('${username}_${const Uuid().v4()}')
         .putFile(profilePic);
     final url = await task.ref.getDownloadURL();
     state = await AsyncValue.guard(() => authService.signUp(
